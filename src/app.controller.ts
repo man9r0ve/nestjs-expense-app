35e82027c6a5628406ab206c84a3b7cbf5e20da4
man9r0ve <mangrove.sh@gmail.com>
2023-06-03 01:30:06 +0900

    2023-06-03 commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Post, Delete, Put, Param } from "@nestjs/common"
+import { Controller, Get, Post, Delete, Put, Param, Body, HttpCode } from "@nestjs/common"
+import { report } from "process"
 import { data, ReportType } from 'src/data'
+import { v4 as uuid } from 'uuid'
 
 @Controller("report/:type")
 export class AppController {
@@ -20,18 +22,47 @@ export class AppController {
   }
 
   @Post()
-  createReport() {
-    return "Created"
+  createReport(@Body() { amount, source }: { amount: number; source: string }, @Param('type') type: string) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    }
+
+    data.report.push(newReport)
+    return newReport
   }
 
   @Put(':id')
-  updateReport() {
-    return "Updated"
+  updateReport(@Param('type') type: string, @Param('id') id: string, @Body() body: {amount: number; source: string}) {
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    const reportToUpdate = data.report.filter(report => report.type === reportType).find(report => report.id === id)
+
+    if(!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(report => report.id === reportToUpdate.id)
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body
+    }
+
+    return data.report[reportIndex]
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
-    return "Deleted"
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex(report => report.id === id)
+
+    if(reportIndex == -1) return;
+
+    data.report.slice(reportIndex, 1)
+
+    return;
   }
 
 }
